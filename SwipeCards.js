824336e766cccac0731e63b692f79eb6992de4a7
wkh237 <xeiyan@gmail.com>
2016-08-14 16:19:19 +0800

    Add missing comma

--- SwipeCards.js
+++ SwipeCards.js
@@ -60,7 +60,7 @@ class SwipeCards extends Component {
 
     this.state = {
       pan: new Animated.ValueXY(),
-      enter: new Animated.Value(0.5)
+      enter: new Animated.Value(0.5),
       card: this.props.cards ? this.props.cards[0] : null,
     }
   }
