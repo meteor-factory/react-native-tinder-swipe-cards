2b9a0866c66661c672fd0eb3ae341575680b5d81
Ben Jones <benjaminpeterjonesesquire@gmail.com>
2017-10-21 12:34:49 +0200

    wip

---
+++ examples/simple/App.js
@@ -0,0 +1,87 @@
+// SwipeCards.js
+'use strict';
+
+import React, { Component } from 'react';
+import {StyleSheet, Text, View, Image} from 'react-native';
+
+import SwipeCards from 'react-native-swipe-cards';
+
+let Card = React.createClass({
+  render() {
+    return (
+      <View style={[styles.card, {backgroundColor: this.props.backgroundColor}]}>
+        <Text>{this.props.text}</Text>
+      </View>
+    )
+  }
+})
+
+class NoMoreCards extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View>
+        <Text style={styles.noMoreCardsText}>No more cards</Text>
+      </View>
+    )
+  }
+}
+
+const Cards = [
+  {text: 'Tomato', backgroundColor: 'red'},
+  {text: 'Aubergine', backgroundColor: 'purple'},
+  {text: 'Courgette', backgroundColor: 'green'},
+  {text: 'Blueberry', backgroundColor: 'blue'},
+  {text: 'Umm...', backgroundColor: 'cyan'},
+  {text: 'orange', backgroundColor: 'orange'},
+]
+
+export default React.createClass({
+  getInitialState() {
+    return {
+      cards: Cards
+    }
+  },
+  handleYup (card) {
+    console.log(`Yup for ${card.text}`)
+  },
+  handleNope (card) {
+    console.log(`Nope for ${card.text}`)
+  },
+  handleMaybe (card) {
+    console.log(`Maybe for ${card.text}`)
+  },
+  render() {
+    // If you want a stack of cards instead of one-per-one view, activate stack mode
+    // stack={true}
+    return (
+      <SwipeCards
+        cards={this.state.cards}
+
+        renderCard={(cardData) => <Card {...cardData} />}
+        renderNoMoreCards={() => <NoMoreCards />}
+
+        handleYup={this.handleYup}
+        handleNope={this.handleNope}
+        handleMaybe={this.handleMaybe}
+        hasMaybeAction
+      />
+    )
+  }
+})
+
+const styles = StyleSheet.create({
+  card: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 300,
+    height: 300,
+  },
+  noMoreCardsText: {
+    fontSize: 22,
+  }
+})
