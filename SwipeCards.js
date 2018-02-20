36f01ded53283a1b167f6d3dd2a01c4ecdf663fc
vendicto <delon-92@yandex.ru>
2018-02-20 10:29:57 +0200

    Update SwipeCards.js

--- SwipeCards.js
+++ SwipeCards.js
@@ -458,7 +458,7 @@ export default class SwipeCards extends Component {
 
     let animatedCardStyles = { transform: [{ translateX }, { translateY }, { rotate }, { scale }], opacity };
 
-    return <Animated.View key={"top"} style={[styles.card, animatedCardStyles]} {...this._panResponder.panHandlers}>
+    return <Animated.View key={"top"} style={[styles.card, animatedCardStyles]} {... this._panResponder.panHandlers}>
       {this.props.renderCard(this.state.card)}
     </Animated.View>;
   }
